717f020b9dd04d2fb30382bbade2a035cd4569d5
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-09-10 19:11:31 +0300

    Добавляем логирование для API автотестов

--- tools/assertions/users.py
+++ tools/assertions/users.py
@@ -7,6 +7,9 @@ from clients.users.users_schema import (
     CreateUserResponseSchema,
     GetUserResponseSchema
 )
+from tools.logger import get_logger
+
+logger = get_logger("USERS_ASSERTIONS")
 
 
 @allure.step("Check create user")
@@ -18,6 +21,8 @@ def assert_create_user_response(request: CreateUserRequestSchema, response: Crea
     :param response: Ответ API с данными пользователя.
     :raises AssertionError: Если хотя бы одно поле не совпадает.
     """
+    logger.info("Check create user response")
+
     assert_equal(response.user.email, request.email, "email")
     assert_equal(response.user.last_name, request.last_name, "last_name")
     assert_equal(response.user.first_name, request.first_name, "first_name")
@@ -33,6 +38,8 @@ def assert_user(actual: UserSchema, expected: UserSchema):
     :param expected: Ожидаемые данные пользователя.
     :return: AssertionError: Если хотя бы одно поле не совпадает.
     """
+    logger.info("Check user")
+
     assert_equal(actual.id, expected.id, "id")
     assert_equal(actual.email, expected.email, "email")
     assert_equal(actual.last_name, expected.last_name, "last_name")
@@ -52,4 +59,6 @@ def assert_get_user_response(
     :param create_user_response: Ответ при создании пользователя.
     :return: AssertionError: Если не совпадают.
     """
+    logger.info("Check get user response")
+
     assert_user(get_user_response.user, create_user_response.user)
