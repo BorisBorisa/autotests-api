0ac8bd4d301008c8f94be7326f2dbf0423207144
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-09-12 18:05:59 +0300

    Практикуемся в интеграции покрытия в API тесты

--- clients/authentication/authentication_client.py
+++ clients/authentication/authentication_client.py
@@ -1,6 +1,7 @@
 import allure
 from httpx import Response
 
+from clients.api_coverage import tracker
 from clients.api_client import APIClient
 from clients.public_http_builder import get_public_http_client
 from clients.authentication.authentication_schema import (
@@ -17,6 +18,7 @@ class AuthenticationClient(APIClient):
     """
 
     @allure.step("Authenticate user")
+    @tracker.track_coverage_httpx(f"{APIRoutes.AUTHENTICATION}/login")
     def login_api(self, request: LoginRequestSchema) -> Response:
         """
         Метод выполняет аутентификацию пользователя.
@@ -30,6 +32,7 @@ class AuthenticationClient(APIClient):
         )
 
     @allure.step("Refresh authentication token")
+    @tracker.track_coverage_httpx(f"{APIRoutes.AUTHENTICATION}/refresh")
     def refresh_api(self, request: RefreshRequestSchema) -> Response:
         """
         Метод обновляет токен авторизации.
